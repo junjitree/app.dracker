60c0439e54d4eb430299c43755a26a7da6c31d6f
junji <juliusj.puno@gmail.com>
2026-03-06 20:34:38 +0800

    Given enough eyeballs, all bugs are shallow. - Linus Torvalds

--- src/pages/trackers/IndexPage.vue
+++ src/pages/trackers/IndexPage.vue
@@ -9,19 +9,38 @@
     <template #actions>
       <q-btn icon="add" to="/trackers/new" color="primary" outline />
     </template>
+
+    <template #body-cell-actions="{ row }">
+      <q-td :class="`text-${row.align}`">
+        <q-btn
+          :disable="loading"
+          icon="delete"
+          color="red"
+          dense
+          flat
+          @click.stop="rowDestroy(row)"
+        >
+          <q-tooltip anchor="bottom middle" self="top middle">Delete</q-tooltip>
+        </q-btn>
+      </q-td>
+    </template>
   </AppTable>
 </template>
 
 <script setup lang="ts">
-import type { QTableColumn } from 'quasar';
+import { useQuasar, type QTableColumn } from 'quasar';
+import { api } from 'src/boot/axios';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 interface Row {
   id: number;
 }
+
+const $q = useQuasar();
 const trigger = ref(0);
 const router = useRouter();
+const loading = ref(false);
 
 const columns: QTableColumn[] = [
   {
@@ -63,12 +82,41 @@ const columns: QTableColumn[] = [
     label: 'Updated',
     sortable: true,
   },
+  {
+    name: 'actions',
+    field: 'actions',
+    label: 'Actions',
+    align: 'center',
+  },
 ];
 
 const rowClick = (evt: MouseEvent, { id }: Row) => {
   evt.preventDefault();
   router.push(`/trackers/${id}`).catch((err) => {
     console.error(err);
+  });
+};
+
+const rowDestroy = ({ id }: Row) => {
+  $q.dialog({
+    title: 'Sure?',
+    message: 'Delete Tracker',
+    ok: 'Confirm',
+    cancel: 'Cancel',
+    persistent: true,
+  }).onOk(() => {
+    loading.value = true;
+    api
+      .delete(`/v1/trackers/${id}`)
+      .then(() => {
+        trigger.value++;
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        loading.value = false;
+      });
   });
 };
 </script>
