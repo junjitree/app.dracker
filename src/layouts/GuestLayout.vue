6014211d89dd43de4d0acfb2ea3f3100601914ae
junji <juliusj.puno@gmail.com>
2026-03-08 14:29:01 +0800

    Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it. - Brian W. Kernighan

--- src/layouts/GuestLayout.vue
+++ src/layouts/GuestLayout.vue
@@ -14,9 +14,7 @@
         >
           <div class="login-inner">
             <div class="text-center q-mb-xl">
-              <h4 class="q-ma-none">
-                {{ appName }}
-              </h4>
+              <img :alt="appName" src="~assets/logo.svg" width="20%" />
             </div>
             <router-view />
           </div>
